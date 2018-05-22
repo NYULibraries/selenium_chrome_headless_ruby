48c003af0375bb9ea31ecd930f6785e867ed7dd5
Eric Griffis <eric.griffis@nyu.edu>
2018-05-22 12:53:56 -0400

    Use sh instead of bash in shell script

--- build_dockers.sh
+++ build_dockers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/sh -ex
 
 docker build . --build-arg RUBY_VERSION=2.4 -t nyulibraries/selenium_chrome_headless_ruby:2.4
 docker build . --build-arg RUBY_VERSION=2.5 -t nyulibraries/selenium_chrome_headless_ruby:2.5
