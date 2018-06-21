038e3d4c5c98374fd46e021d3a8a88421f16466b
Eric Griffis <eric.griffis@nyu.edu>
2018-06-21 17:38:39 -0400

    Add slim dockerfile

--- build_dockers.sh
+++ build_dockers.sh
@@ -2,5 +2,10 @@
 
 docker build . --build-arg RUBY_VERSION=2.4 -t nyulibraries/selenium_chrome_headless_ruby:2.4
 docker build . --build-arg RUBY_VERSION=2.5 -t nyulibraries/selenium_chrome_headless_ruby:2.5
+docker build . -f Dockerfile.slim --build-arg RUBY_VERSION=2.4 -t nyulibraries/selenium_chrome_headless_ruby:2.4-slim
+docker build . -f Dockerfile.slim --build-arg RUBY_VERSION=2.5 -t nyulibraries/selenium_chrome_headless_ruby:2.5-slim
+
 docker push nyulibraries/selenium_chrome_headless_ruby:2.4
 docker push nyulibraries/selenium_chrome_headless_ruby:2.5
+docker push nyulibraries/selenium_chrome_headless_ruby:2.4-slim
+docker push nyulibraries/selenium_chrome_headless_ruby:2.5-slim
