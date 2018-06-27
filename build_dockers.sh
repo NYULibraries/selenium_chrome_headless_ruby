629e02d18bb2ebb844a04cf7892d9bf304315ae0
Eric Griffis <eric.griffis@nyu.edu>
2018-06-27 12:30:16 -0400

    Add ruby 2.3 support

--- build_dockers.sh
+++ build_dockers.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -ex
 
+docker build . --build-arg RUBY_VERSION=2.3 -t nyulibraries/selenium_chrome_headless_ruby:2.3
 docker build . --build-arg RUBY_VERSION=2.4 -t nyulibraries/selenium_chrome_headless_ruby:2.4
 docker build . --build-arg RUBY_VERSION=2.5 -t nyulibraries/selenium_chrome_headless_ruby:2.5
+docker build . -f Dockerfile.slim --build-arg RUBY_VERSION=2.3 -t nyulibraries/selenium_chrome_headless_ruby:2.3-slim
 docker build . -f Dockerfile.slim --build-arg RUBY_VERSION=2.4 -t nyulibraries/selenium_chrome_headless_ruby:2.4-slim
 docker build . -f Dockerfile.slim --build-arg RUBY_VERSION=2.5 -t nyulibraries/selenium_chrome_headless_ruby:2.5-slim
 
+docker push nyulibraries/selenium_chrome_headless_ruby:2.3
 docker push nyulibraries/selenium_chrome_headless_ruby:2.4
 docker push nyulibraries/selenium_chrome_headless_ruby:2.5
+docker push nyulibraries/selenium_chrome_headless_ruby:2.3-slim
 docker push nyulibraries/selenium_chrome_headless_ruby:2.4-slim
 docker push nyulibraries/selenium_chrome_headless_ruby:2.5-slim
