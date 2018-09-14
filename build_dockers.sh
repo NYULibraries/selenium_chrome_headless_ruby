2e95164ea8352995f87a107d444f609b652101be
Eric Griffis <eric.griffis@nyu.edu>
2018-09-14 11:56:49 -0400

    Fix versioning

--- build_dockers.sh
+++ build_dockers.sh
@@ -4,8 +4,8 @@ docker-compose build
 
 docker tag selenium_chrome_headless_ruby:2.3.7 nyulibraries/selenium_chrome_headless_ruby:2.3.7
 docker tag selenium_chrome_headless_ruby:2.4.4 nyulibraries/selenium_chrome_headless_ruby:2.4.4
-docker tag selenium_chrome_headless_ruby:2.3.7 nyulibraries/selenium_chrome_headless_ruby:2.5.1
-docker tag selenium_chrome_headless_ruby:2.5.1-slim nyulibraries/selenium_chrome_headless_ruby:2.3.7-slim
+docker tag selenium_chrome_headless_ruby:2.5.1 nyulibraries/selenium_chrome_headless_ruby:2.5.1
+docker tag selenium_chrome_headless_ruby:2.3.7-slim nyulibraries/selenium_chrome_headless_ruby:2.3.7-slim
 docker tag selenium_chrome_headless_ruby:2.4.4-slim nyulibraries/selenium_chrome_headless_ruby:2.4.4-slim
 docker tag selenium_chrome_headless_ruby:2.5.1-slim nyulibraries/selenium_chrome_headless_ruby:2.5.1-slim
 
