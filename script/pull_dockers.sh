5b4fbe8b63f5abd0e1d6a676ee3de79a3b0b06d6
Eric Griffis <eric.griffis@nyu.edu>
2018-09-18 13:26:16 -0400

    Tag docker images with branch; tag main images only on master

--- script/pull_dockers.sh
+++ script/pull_dockers.sh
@@ -2,5 +2,5 @@
 
 for version in 2.3.7 2.4.4 2.5.1 2.3.7-slim 2.4.4-slim 2.5.1-slim
 do
-  docker pull nyulibraries/selenium_chrome_headless_ruby:$version
+  docker pull nyulibraries/selenium_chrome_headless_ruby:$version-${CIRCLE_BRANCH//\//_} || docker pull nyulibraries/selenium_chrome_headless_ruby:$version
 done
--- script/push_dockers.sh
+++ script/push_dockers.sh
@@ -2,12 +2,20 @@
 
 for version in 2.3.7 2.4.4 2.5.1 2.3.7-slim 2.4.4-slim 2.5.1-slim
 do
-  docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version
-  docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version-${CIRCLE_SHA1}
+  docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version-${CIRCLE_BRANCH//\//_}
+  docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  if [[ "$CIRCLE_BRANCH" = "master" ]]
+  then
+    docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version
+  fi
 done
 
 for version in 2.3.7 2.4.4 2.5.1 2.3.7-slim 2.4.4-slim 2.5.1-slim
 do
-  docker push nyulibraries/selenium_chrome_headless_ruby:$version
-  docker push nyulibraries/selenium_chrome_headless_ruby:$version-${CIRCLE_SHA1}
+  docker push nyulibraries/selenium_chrome_headless_ruby:$version-${CIRCLE_BRANCH//\//_}
+  docker push nyulibraries/selenium_chrome_headless_ruby:$version-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  if [[ "$CIRCLE_BRANCH" = "master" ]]
+  then
+    docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version
+  fi
 done
