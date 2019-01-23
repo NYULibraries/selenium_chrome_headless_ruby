15c8f4692d3e7be5cdecc6fc35d827325f49960b
Eric Griffis <eric.griffis@nyu.edu>
2019-01-23 14:02:07 -0500

    Circle - push images to quay

--- script/push_dockers.sh
+++ script/push_dockers.sh
@@ -2,25 +2,25 @@
 
 for version in 2.3.7 2.4.4 2.5.1 2.3.7-slim 2.4.4-slim 2.5.1-slim
 do
-  docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}
-  docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  docker tag selenium_chrome_headless_ruby:$version quay.io/nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}
+  docker tag selenium_chrome_headless_ruby:$version quay.io/nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
   if [[ "$CIRCLE_BRANCH" = "master" ]]
   then
-    docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69
+    docker tag selenium_chrome_headless_ruby:$version quay.io/nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69
     # also tag with short version, which excludes patch version number
     short_version=`echo $version | sed -e 's/\..$//g' | sed -e 's/\..-/-/g'`
-    docker tag selenium_chrome_headless_ruby:$version nyulibraries/selenium_chrome_headless_ruby:$short_version-chrome_69
+    docker tag selenium_chrome_headless_ruby:$version quay.io/nyulibraries/selenium_chrome_headless_ruby:$short_version-chrome_69
   fi
 done
 
 for version in 2.3.7 2.4.4 2.5.1 2.3.7-slim 2.4.4-slim 2.5.1-slim
 do
-  docker push nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}
-  docker push nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
+  docker push quay.io/nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}
+  docker push quay.io/nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69-${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
   if [[ "$CIRCLE_BRANCH" = "master" ]]
   then
-    docker push nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69
+    docker push quay.io/nyulibraries/selenium_chrome_headless_ruby:$version-chrome_69
     short_version=`echo $version | sed -e 's/\..$//g' | sed -e 's/\..-/-/g'`
-    docker push nyulibraries/selenium_chrome_headless_ruby:$short_version-chrome_69
+    docker push quay.io/nyulibraries/selenium_chrome_headless_ruby:$short_version-chrome_69
   fi
 done
